cd4adcb3e42c682e4508932d0b64258bb01ec9ad
Olawande Akinmosin <samueldacoal@gmail.com>
2024-07-30 08:49:47 +0100

    UI Updates

--- src/components/NavMobile.tsx
+++ src/components/NavMobile.tsx
@@ -1,31 +1,39 @@
-import { X } from "lucide-react";
 import Nav from "./Nav";
 import React, { SetStateAction } from "react";
 import NavSelect from "./NavSelect";
 
 const NavMobile = ({
 	setOpenNav,
+	openNav,
 }: {
 	setOpenNav: React.Dispatch<SetStateAction<boolean>>;
+	openNav: boolean;
 }) => {
 	return (
-		<section className="fixed inset-0 isolate flex h-full flex-col bg-white/60 backdrop-blur-lg lg:hidden">
-			<div className="flex justify-center">
-				<X
-					className="text-center text-black"
-					size={60}
-					onClick={() => setOpenNav(false)}
-				/>
-			</div>
-			<div className="grid flex-1 place-items-center">
-				<div className="">
-					<Nav />
-				</div>
-				<div className="flex-1">
+		<div>
+			{/* Hamburger menu button */}
+
+			{/* Sidebar */}
+			<div
+				className={` fixed left-0 top-0 z-50 h-full w-64 bg-accent p-5 text-white transition-transform duration-300 ease-in-out${
+					openNav ? "translate-x-0" : "-translate-x-full"
+				}`}
+			>
+				<h2 className="mb-5 text-3xl font-bold">Menu</h2>
+				<div className="my-6 flex-1">
 					<NavSelect isLight={false} />
 				</div>
+				<Nav />
 			</div>
-		</section>
+
+			{/* Overlay */}
+			{openNav && (
+				<div
+					className="fixed inset-0 z-40 bg-black/50"
+					onClick={() => setOpenNav(false)}
+				></div>
+			)}
+		</div>
 	);
 };
 export default NavMobile;
